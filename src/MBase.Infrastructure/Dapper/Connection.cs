225ad5e328a4ecf0d82764eeaff4ac707cbb4bbe
Yusuf UZUN <yusuf.uzun@outlook.com>
2024-09-04 23:05:40 +0300

    IConnection interface is moved

--- src/MBase.Infrastructure/Dapper/Connection.cs
+++ src/MBase.Infrastructure/Dapper/Connection.cs
@@ -4,18 +4,6 @@ using Dapper;
 
 namespace MBase.Infrastructure.Dapper
 {
-    public interface IConnection<TDbConnection> where TDbConnection : DbConnection
-    {
-        IDbConnection GetConnection();
-        Connection<TDbConnection> SetConnectionString(string connectionString);
-        Task<IEnumerable<T>> QueryAsync<T>(string query);
-        Task<IEnumerable<T>> QueryAsync<T>(string query, object parameters);
-        Task<int> ExecuteAsync(string query);
-        Task<int> ExecuteAsync(string query, object parameters);
-        Task OpenAsync();
-        Task CloseAsync();
-    }
-
     public class Connection<TDbConnection>(TDbConnection dbConnection) : IConnection<TDbConnection> where TDbConnection : DbConnection
     {
         public string ConnectionString => dbConnection.ConnectionString;
---
+++ src/MBase.Infrastructure/Dapper/IConnection.cs
@@ -0,0 +1,16 @@
+﻿using System.Data;
+using System.Data.Common;
+
+namespace MBase.Infrastructure.Dapper;
+
+public interface IConnection<TDbConnection> where TDbConnection : DbConnection
+{
+    IDbConnection GetConnection();
+    Connection<TDbConnection> SetConnectionString(string connectionString);
+    Task<IEnumerable<T>> QueryAsync<T>(string query);
+    Task<IEnumerable<T>> QueryAsync<T>(string query, object parameters);
+    Task<int> ExecuteAsync(string query);
+    Task<int> ExecuteAsync(string query, object parameters);
+    Task OpenAsync();
+    Task CloseAsync();
+}
