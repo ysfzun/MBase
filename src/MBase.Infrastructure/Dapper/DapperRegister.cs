e641d07b5d115369597ec5c391e63d27dd05260e
Yusuf UZUN <yusuf.uzun@outlook.com>
2024-09-13 22:49:15 +0300

    UseDapper extension is added for IDapperService registration and also test postgresql connection added to Development.json

---
+++ src/MBase.Infrastructure/Dapper/DapperRegister.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MBase.Infrastructure.Dapper
+{
+    public static class DapperRegister
+    {
+        public static void UseDapper(this IServiceCollection services)
+        {
+            var baseType = typeof(IDapperService);
+
+            var assembly = Assembly.GetEntryAssembly();
+
+            if (assembly == null)
+                assembly = Assembly.GetCallingAssembly();
+
+            var types = assembly.GetTypes()
+                .Where(t => t is { IsClass: true, IsAbstract: false } && baseType.IsAssignableFrom(t));
+
+            foreach (var derivedType in types)
+            {
+                var interfaceType = derivedType
+                    .GetInterfaces()
+                    .FirstOrDefault(i => i != typeof(IDapperService) && baseType.IsAssignableFrom(i));
+
+                if (interfaceType != null)
+                    services.AddScoped(interfaceType, derivedType);
+            }
+        }
+    }
+}
